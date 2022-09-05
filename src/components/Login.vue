d729a0cd55f46f2b0a99e941fb89df0adde8c568
FreeReol <2571219329@qq.com>
2022-09-05 20:58:05 +0800

    add files

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,19 +1,23 @@
 <template>
   <div class="login_container">
     <div class="login_box">
-      <!-- 头像区 -->
+      <!-- 头像区域 -->
       <div class="avatar_box">
         <img src="../assets/logo.png" alt />
       </div>
-      <!-- 登录表单 -->
-      <el-form  label-width="0px" class="login_form">
+      <!-- 登录表单区域 -->
+      <el-form :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
         <!-- 用户名 -->
-        <el-form-item >
-          <el-input ></el-input>
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
         </el-form-item>
         <!-- 密码 -->
-        <el-form-item >
-          <el-input ></el-input>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
@@ -26,7 +30,31 @@
 </template>
 
 <script>
-export default {};
+import { password } from "../../vue_api_server/test/configs/config";
+export default {
+  data() {
+    return {
+      // 登录表单的数据绑定对象
+      loginForm: {
+        username: "",
+        password: ""
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 15, message: "长度在 6 到 15 个字符", trigger: "blur" }
+        ]
+      }
+    };
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -64,7 +92,7 @@ export default {};
   }
 }
 
-.login_form{
+.login_form {
   position: absolute;
   bottom: 0;
   width: 100%;
@@ -72,7 +100,7 @@ export default {};
   box-sizing: border-box;
 }
 
-.btns{
+.btns {
   display: flex;
   justify-content: flex-end;
 }
