549747dc93946e4027ebf01e1e000db60d092e3f
FreeReol <2571219329@qq.com>
2022-09-06 12:10:01 +0800

    add files

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,13 @@
         <img src="../assets/logo.png" alt />
       </div>
       <!-- 登录表单区域 -->
-      <el-form :rules="loginFormRules" :model="loginForm" label-width="0px" class="login_form">
+      <el-form
+        ref="loginFormRef"
+        :rules="loginFormRules"
+        :model="loginForm"
+        label-width="0px"
+        class="login_form"
+      >
         <!-- 用户名 -->
         <el-form-item prop="username">
           <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
@@ -21,8 +27,8 @@
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -30,7 +36,6 @@
 </template>
 
 <script>
-import { password } from "../../vue_api_server/test/configs/config";
 export default {
   data() {
     return {
@@ -53,6 +58,29 @@ export default {
         ]
       }
     };
+  },
+  methods: {
+    // 点击重置按钮，重置登录表单
+    resetLoginForm() {
+      // console.log(this)
+      this.$refs.loginFormRef.resetFields();
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return;
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        // console.log(res);
+        if (res.meta.status !== 200) return this.$message.error("登录失败！");
+        this.$message.success("登录成功！");
+        // 1. 将登录成功之后的 token,保存到客户端的 sessionStorage(会话机制/只在当前页面生效)中 localStorage(持久话机制/关闭页面也不会忘记数据)
+        //   1.1 项目中除了登录之外的API接口,必须在登录之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效, 所以将 token 保存在 sessionStorage中
+        window.sessionStorage.setItem("token", res.data.token);
+        // 2. 通过编程式路由导航跳转到后台主页,路由地址是 /home
+        this.$router.push("/home");
+        this.loginLoading = false;
+      });
+    }
   }
 };
 </script>
