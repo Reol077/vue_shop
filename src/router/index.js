a146ee533b3870af4f7c26770eedd6d8215dbe90
FreeReol <2571219329@qq.com>
2022-09-06 20:53:13 +0800

    用户管理模块完成了一半

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 
 
 Vue.use(VueRouter)
@@ -9,7 +11,12 @@ Vue.use(VueRouter)
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
-  { path: '/home', component: Home },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [{ path: '/welcome', component: Welcome },{path:'/users',component:Users}]
+  },
 ]
 
 const router = new VueRouter({
@@ -17,16 +24,16 @@ const router = new VueRouter({
 })
 
 // 挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   // to代表将要访问的路径
   // from代表从哪个路径来
   // next是一个函数，表示放行
   //    next()放行 next('/login) 强制跳转
 
-  if(to.path === '/login') return next()
+  if (to.path === '/login') return next()
   // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login')
+  if (!tokenStr) return next('/login')
   next()
 })
 
